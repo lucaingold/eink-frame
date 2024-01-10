bb5abbac524e57f3a78c27ba43983c77db53c78d
Luca Ingold <luca.ingold@gmail.com>
2024-01-10 23:00:39 +0100

    html server

---
+++ pic/html/app.js
@@ -0,0 +1,84 @@
+const container = document.querySelector('.container');
+
+function generateMasonryGrid(columns, posts) {
+  container.innerHTML = '';
+
+  //Store column arrays that contain relevant posts
+  let columnWrappers = {};
+
+  //Create column item array and  add this to column wrapper object
+  for (let i = 0; i < columns; i++) {
+    columnWrappers[`column${i}`] = [];
+  }
+  for (let i = 0; i < posts.length; i++) {
+    const column = i % columns;
+    columnWrappers[`column${column}`].push(posts[i]);
+  }
+  for (let i = 0; i < columns; i++) {
+    let columnPosts = columnWrappers[`column${i}`];
+    let column = document.createElement('div');
+    column.classList.add('column');
+    columnPosts.forEach((posts) => {
+      let postDiv = document.createElement('div');
+      postDiv.classList.add('post');
+      postDiv.setAttribute('onclick', `showImage('${posts.path}')`);
+      let image = document.createElement('img');
+      image.src = posts.image;
+      let overlay = document.createElement('div');
+      overlay.classList.add('overlay');
+      let title = document.createElement('h3');
+      title.innerText = posts.title;
+
+      overlay.appendChild(title);
+      postDiv.append(image, overlay);
+      column.appendChild(postDiv);
+    });
+    container.appendChild(column);
+  }
+}
+
+function showImage(path) {
+      fetch('http://inkframe.local:8080/your-post-endpoint', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/plain', 
+        },
+        body: path,
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+}
+
+let previousScreenSize = innerWidth;
+console.log(previousScreenSize);
+
+window.addEventListener('resize', () => {
+  imageIndex = 0;
+  if (innerWidth < 600 && previousScreenSize >= 600) {
+    generateMasonryGrid(1, posts);
+  } else if (
+    innerWidth >= 600 &&
+    innerWidth < 1000 &&
+    (previousScreenSize < 600 || previousScreenSize >= 1000)
+  ) {
+    generateMasonryGrid(2, posts);
+  } else if (innerWidth >= 1000 && previousScreenSize < 1000) {
+    generateMasonryGrid(4, posts);
+  }
+  previousScreenSize = innerWidth;
+});
+
+//Page Load
+if (previousScreenSize < 600) {
+  generateMasonryGrid(1, posts);
+} else if (previousScreenSize >= 600 && previousScreenSize < 1000) {
+  generateMasonryGrid(2, posts);
+} else {
+  generateMasonryGrid(4, posts);
+}
