47f720cb1f25e210773d10607fd9c7f407a9c1ea
Luca Ingold <luca.ingold@gmail.com>
2024-01-11 00:17:32 +0100

    f

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/luca/code/eink-frame
-/usr/bin/python3 runWebserverImages.py > /dev/null &
+/usr/bin/python3 -m http.server 80 > /dev/null &
 /home/luca/code/eink-frame/epd -2.27 0 &
