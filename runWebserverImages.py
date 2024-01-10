dcaecb8b36533a4062f837c0abf59950825f01c5
Luca Ingold <luca.ingold@gmail.com>
2024-01-11 00:30:33 +0100

    f

--- runWebserverImages.py
+++ runWebserverImages.py
@@ -3,8 +3,9 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
+CORS(app, resources={r"/*": {"origins": "http://0.0.0.0:8080"}})
 
 # Your existing routes and server setup here
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8000)
