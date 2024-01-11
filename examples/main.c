779de8ad8d5a2a9b3d9a8951aabcbecfa54dbe91
Luca Ingold <luca.ingold@gmail.com>
2024-01-11 13:44:49 +0100

    initscreen

--- examples/main.c
+++ examples/main.c
@@ -69,37 +69,7 @@ int answer_to_connection(void *cls, struct MHD_Connection *connection,
 
 void start_http_server();
 
-int main(int argc, char *argv[])
-{
-    //Exception handling:ctrl + c
-    signal(SIGINT, Handler);
-
-    if (argc < 2){
-        Debug("Please input VCOM value on FPC cable!\r\n");
-        Debug("Example: sudo ./epd -2.51\r\n");
-        exit(1);
-    }
-	if (argc != 3){
-		Debug("Please input e-Paper display mode!\r\n");
-		Debug("Example: sudo ./epd -2.27 0 or sudo ./epd -2.51 1\r\n");
-		Debug("Now, 10.3 inch glass panle is mode1, else is mode0\r\n");
-		Debug("If you don't know what to type in just type 0 \r\n");
-		exit(1);
-    }
-
-    //Init the BCM2835 Device
-    if(DEV_Module_Init()!=0){
-        return -1;
-    }
-
-    double temp;
-    sscanf(argv[1],"%lf",&temp);
-    VCOM = (UWORD)(fabs(temp)*1000);
-    Debug("VCOM value:%d\r\n", VCOM);
-	sscanf(argv[2],"%d",&epd_mode);
-    Debug("Display mode:%d\r\n", epd_mode);
-    Dev_Info = EPD_IT8951_Init(VCOM);
-
+void  initScreen(){
     //get some important info from Dev_Info structure
     Panel_Width = Dev_Info.Panel_W;
     Panel_Height = Dev_Info.Panel_H;
@@ -129,6 +99,39 @@ int main(int argc, char *argv[])
     Debug("A2 Mode:%d\r\n", A2_Mode);
 
 	EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, INIT_Mode);
+}
+
+int main(int argc, char *argv[])
+{
+    //Exception handling:ctrl + c
+    signal(SIGINT, Handler);
+
+    if (argc < 2){
+        Debug("Please input VCOM value on FPC cable!\r\n");
+        Debug("Example: sudo ./epd -2.51\r\n");
+        exit(1);
+    }
+	if (argc != 3){
+		Debug("Please input e-Paper display mode!\r\n");
+		Debug("Example: sudo ./epd -2.27 0 or sudo ./epd -2.51 1\r\n");
+		Debug("Now, 10.3 inch glass panle is mode1, else is mode0\r\n");
+		Debug("If you don't know what to type in just type 0 \r\n");
+		exit(1);
+    }
+
+    double temp;
+    sscanf(argv[1],"%lf",&temp);
+    VCOM = (UWORD)(fabs(temp)*1000);
+    Debug("VCOM value:%d\r\n", VCOM);
+	sscanf(argv[2],"%d",&epd_mode);
+    Debug("Display mode:%d\r\n", epd_mode);
+    Dev_Info = EPD_IT8951_Init(VCOM);
+
+    //Init the BCM2835 Device
+    if(DEV_Module_Init()!=0){
+        return -1;
+    }
+
 
     start_http_server();  // Start the HTTP server
 
@@ -211,6 +214,7 @@ int answer_to_connection(void *cls, struct MHD_Connection *connection,
     if (strncmp(post_data, "path=", 5) == 0) {
         const char *path = post_data + 5;  // Skip "path="
         printf("Path is: %s\n", path);
+        initScreen();
         EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, GC16_Mode);
         Display_BMP_WITH_PATH(Panel_Width, Panel_Height, Init_Target_Memory_Addr, BitsPerPixel_4, path);
     } else {
